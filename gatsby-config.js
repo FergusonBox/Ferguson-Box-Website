1afa63890bc50cfe1fcc140f8305208ba47489b1
Adam Patyk <apatyk@g.clemson.edu>
2020-05-28 16:27:51 -0400

    Adding Google Fonts support

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,17 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`]
+          },
+        ],
+      },
+    },
+    {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
       options: {
