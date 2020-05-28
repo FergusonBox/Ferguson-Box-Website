f3944c864bbceba0280cafe3ffae6e0882bbc9d1
Adam Patyk <apatyk@g.clemson.edu>
2020-05-28 15:52:33 -0400

    Cleaning up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Ferguson Box Website',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Packaging Done Right',
+    keywords:
+      'packaging, boxes, ferguson box, ferguson, box, corrugated, cardboard, packaging supplies'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
