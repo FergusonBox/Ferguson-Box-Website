f50c71208efd8fea540688a0b284b8c78e880b8f
Adam Patyk <apatyk@g.clemson.edu>
2020-05-29 11:03:27 -0400

    Creating navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import { Location } from '@reach/router'
+
+import './Navbar.scss'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -35,61 +37,53 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar"
         role="navigation"
         aria-label="main-navigation"
       >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
+        <Location>
+          {({ location: { pathname } }) => (
+            <>
+              <div className="navbar__brand">
+                <Link to="/" className="navbar__item" title="Logo">
+                  <img src={logo} alt="Ferguson Box" style={{ width: '128px' }} />
+                </Link>
+                {/* Hamburger menu */}
+                <div
+                  className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                  data-target="navMenu"
+                  onClick={() => this.toggleHamburger()}
+                >
+                  <span />
+                  <span />
+                  <span />
+                </div>
+              </div>
+              <div
+                id="navMenu"
+                className={`navbar__menu ${this.state.navBarActiveClass}`}
               >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
+                <div className="navbar__start">
+                  <Link className={`navbar__item ${pathname.includes('company') && 'active'}`} to="/our-company">
+                    OUR COMPANY
+                  </Link>
+                  <Link className={`navbar__item ${pathname.includes('capabilities') && 'active'}`} to="/capabilities">
+                    CAPABILITIES
+                  </Link>
+                  <Link className={`navbar__item ${pathname.includes('services') && 'active'}`} to="/services">
+                    SERVICES
+                  </Link>
+                  <Link className={`navbar__item ${pathname.includes('products') && 'active'}`} to="/products">
+                    PRODUCTS
+                  </Link>
+                  <Link className={`navbar__item ${pathname.includes('contact') && 'active'}`} to="/contact">
+                    CONTACT US
+                  </Link>
+                </div>
+              </div>
+            </>
+          )}
+        </Location>
       </nav>
     )
   }
