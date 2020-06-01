3182724494c4f1e9fc4396ac625d4d6349def005
Adam Patyk <apatyk@g.clemson.edu>
2020-06-01 17:52:32 -0400

    Adding support for linked files in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,12 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+          {
             resolve: 'gatsby-remark-relative-images',
             options: {
               name: 'uploads',
