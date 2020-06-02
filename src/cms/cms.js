1c3f6bf1d7da5e68e2c6db99902143cb99332db7
Adam Patyk <apatyk@g.clemson.edu>
2020-06-02 17:08:57 -0400

    Misc fixes, adding gold to color palette

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,5 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
-CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
