79e37b7a1a060401b3f4d89989f69fd84822d1d0
Adam Patyk <apatyk@g.clemson.edu>
2020-06-03 11:53:58 -0400

    Changing 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,10 @@ import Layout from '../components/Layout'
 const NotFoundPage = () => (
   <Layout>
     <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <h1>PAGE NOT FOUND</h1>
+      <h2>We're Sorry.</h2>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+      <p>The page may have been moved, changed, or removed.</p>
     </div>
   </Layout>
 )
