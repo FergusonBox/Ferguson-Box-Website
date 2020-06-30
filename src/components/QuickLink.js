1104891292ad4dc08c9eabe6340d1505e4f7fc25
Adam Patyk <apatyk@g.clemson.edu>
2020-06-30 17:42:06 -0400

    Removing page links from quick links on homepage

--- src/components/QuickLink.js
+++ src/components/QuickLink.js
@@ -6,23 +6,19 @@ import './QuickLink.scss'
 const QuickLink = ({ 
   title,
   text,
-  link,
   docLink,
   docLinkText 
 }) => (
-  <>
-    <a className="quick-link" href={link}>
+    <div className="quick-link" >
       <h2>{title}</h2>
       <p>{text}</p>
       {docLinkText && <a href={docLink.publicURL} className="quick-link__link">{docLinkText}</a>}
-    </a>
-  </>
+    </div>
 )
 
 QuickLink.propTypes = {
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
   docLink: PropTypes.string,
   docLinkText: PropTypes.string
 }
