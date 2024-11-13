8514f533adb81b811467b011de36c383facd172e
Adam Patyk <adamamp@icloud.com>
2024-11-12 19:20:03 -0700

    Add AiCC certification

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,13 @@ import ISOCert from '../../static/uploads/iso_cert_badge.png'
 import SFICert from '../../static/uploads/sfi_cert_badge.png'
 import WBENCCert from '../../static/uploads/wbenc_cert_badge.png'
 import FSCCert from '../../static/uploads/fsc_cert_badge.png'
+import AiCCcert from '../../static/uploads/aicc_cert_badge.jpg'
 
 import ISOCertDoc from '../../static/uploads/iso_certificate.pdf'
 import SFICertDoc from '../../static/uploads/sfi_certificate.pdf'
 import WBENCCertDoc from '../../static/uploads/wbenc_certificate.pdf'
 import FSCCertDoc from '../../static/uploads/fsc_certificate.pdf'
+import AiCCcertDoc from '../../static/uploads/aicc_certificate.pdf'
 
 import './Header.scss'
 
@@ -53,6 +55,14 @@ const Header = () => {
           rel="noreferrer"
         >
           <img src={ISOCert} alt="ISO 9001 Certified"></img>
+        </a>
+        <a
+          className="header__certifications__item"
+          href={AiCCcertDoc}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={AiCCcert} alt="AiCC Certified"></img>
         </a>
         <a
           className="header__certifications__item"
